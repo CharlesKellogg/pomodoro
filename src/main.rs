94cc0a75760105921bee05ac16e5a882ed9922b7
Charles Kerver Kellogg <CharlesKerverKellogg@gmail.com>
2025-11-09 00:39:41 -0500

    add spans

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::{
     Frame,
     layout::{Constraint, Layout},
-    style::{Style, Stylize},
-    widgets::Block,
+    style::{Color, Modifier, Style, Stylize},
+    text::{Line, Span},
+    widgets::{Block, Paragraph},
 };
 use std::time::Instant;
 use tui_big_text::{BigText, PixelSize};
@@ -60,5 +61,32 @@ fn draw(frame: &mut Frame, remaining_seconds: u64) {
         .centered()
         .lines(vec![remaining_seconds.to_string().red().into()])
         .build();
-    frame.render_widget(big_text, left_area);
+    // frame.render_widget(big_text, left_area);
+
+    let areas = Layout::vertical([Constraint::Length(1); 4]).split(frame.area());
+
+    let line = Line::from(vec![
+        Span::raw("Hello "),
+        Span::styled(
+            "World",
+            Style::new()
+                .fg(Color::Green)
+                .bg(Color::White)
+                .add_modifier(Modifier::BOLD),
+        ),
+        "!".red().on_light_yellow().italic(),
+    ]);
+    // frame.render_widget(line, areas[0]);
+
+    // using the short-hand syntax and implicit conversions
+    let paragraph = Paragraph::new("Hello World!".red().on_white().bold());
+    // frame.render_widget(paragraph, areas[1]);
+
+    // style the whole widget instead of just the text
+    let paragraph = Paragraph::new("Hello World!").style(Style::new().red().on_white());
+    // frame.render_widget(paragraph, areas[2]);
+
+    // use the simpler short-hand syntax
+    let paragraph = Paragraph::new("Hello World!").blue().on_yellow();
+    // frame.render_widget(paragraph, areas[3]);
 }
